e3a8c3b1f302a532c70e5f59e3929c8e46c48aa8
Jakub Walczak <jakub_walczak@tlen.pl>
2023-02-03 01:22:04 +0100

    add constants and weight adding function

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <script setup>
   import { ref, shallowRef, computed, watch, nextTick } from 'vue'
+  import Chart from 'chart.js/auto'
+
+  const weights = ref([])
+  const weightChartEl = ref(null)
+  const weightChart = shallowRef(null)
+  const weightInput = ref(60.0)
+  const currentWeight = computed(() => {
+    return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 }
+    })
+
+  const addWeight = () => {
+    weights.value.push({
+      weight: weightInput.value,
+      date: new Date().getTime()
+    })
+  }
+  
 </script>
 
 <template>
